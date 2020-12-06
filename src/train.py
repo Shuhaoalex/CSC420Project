7f81ff7bca1541696577b109c53ca2d2768bb884
Shuhao Li <shuhaoalex.li@mail.utoronto.ca>
2020-12-05 20:44:54 -0500

    ...

--- src/train.py
+++ src/train.py
@@ -7,13 +7,14 @@ import sys
 import json
 
 # folder = sys.argv[1]
-folder = "job1"
-with open(os.path.join(folder, 'configurations.json'), 'r') as f:
+job_folder = "job1"
+with open(os.path.join(job_folder, 'configurations.json'), 'r') as f:
     config = json.load(f)
 dataset_config = config["dataset_config"]
 model_config = config["model_config"]
+model_config["model_ckpoint_dir"] = os.path.join(job_folder, model_config["model_ckpoint_dir"])
 dataset = Dataset(dataset_config, dataset_config["img_train_flist"], dataset_config["mask_train_flist"], training=True)
 dataset = dataset.dataset.shuffle(100).batch(10).prefetch(200)
 model = InpaitingModel(model_config)
 
-model.train_edge_part()
+# model.train_edge_part()
