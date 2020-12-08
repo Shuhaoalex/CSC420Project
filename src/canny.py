7a4f5d84fc4793803d63913d05fb7de74a392519
Serena Wang <serenachenyue.wang@mail.utoronto.ca>
2020-12-09 00:13:14 +0800

    Use canny from cv2 instead of skimage. Change data return type to uint8 and undo normalization

--- src/canny.py
+++ src/canny.py
@@ -7,8 +7,8 @@ def canny(img, guassian_blur_sigma=1.0, sobel_size=3, lowThresholdRatio=0.05, hi
     """
     g_ksize = int(guassian_blur_sigma * 6) - 1
     blurred = cv.GaussianBlur(img, (g_ksize, g_ksize), guassian_blur_sigma)
-    gx = cv.Sobel(cv.cvtColor(blurred, cv.COLOR_BGR2GRAY), cv.CV_16SC1, 1, 0, ksize=sobel_size)
-    gy = cv.Sobel(cv.cvtColor(blurred, cv.COLOR_BGR2GRAY), cv.CV_16SC1, 0, 1, ksize=sobel_size)
+    gx = cv.Sobel(img, cv.CV_16SC1, 1, 0, ksize=sobel_size)
+    gy = cv.Sobel(img, cv.CV_16SC1, 0, 1, ksize=sobel_size)
     g = np.hypot(gx, gy).max()
     highThreshold = g * highThresholdRatio
     lowThreshold = highThreshold * lowThresholdRatio
